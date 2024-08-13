4958d1a2c9f5e025b8ed2f7bfb46a3fb0ef7f259
ER1-53 <cremey.erwan@gmail.com>
2024-08-13 18:16:44 +0200

    update page.tsx, creat productlist and finish slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Link from "next/link"
 import Image from "next/image"
 
@@ -33,9 +33,18 @@ const slides = [
 
 const Slider = () => {
     const [current, setcurrent] = useState(0)
-    return (
-        <div className="h-[calc(100vh-80px)] overflow-hidden">
-            <div className="w-max h-full flex transition-all easy-in-out duration-1000">
+
+//     useEffect(()=> {
+//     const interval = setInterval(() => {
+//         setcurrent((prev) => (prev === slides.length -1 ? 0 : prev + 1))
+//     }, 3000);
+
+//     return () => clearInterval(interval);
+// }, []);
+return (
+    <div className="h-[calc(100vh-80px)] overflow-hidden">
+        <div className="w-max h-full flex transition-all easy-in-out duration-1000"
+            style={{ transform: `translateX(-${current * 100}vw)`}}>
                 {slides.map(slide=>(
                     <div className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
                         {/* TEXT CONTAINER */}
@@ -52,21 +61,25 @@ const Slider = () => {
                         </div>
                     </div>
                 ))}
-                <div className="">
-                    {slides.map((slide, index)=>(
-                            <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
-                                current === index ? "scale-150" : ""}`}
-                                key={slide.id}
-                            >
-                                {current === index && ( 
-                                    <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
-                                )}
-                            </div>
-                        ))}
-                </div>
-            </div>
         </div>
+            <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+                {slides.map((slide, index)=>(
+                    <div className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${current === index ? "scale-150" : ""}`}
+                    key={slide.id}
+                    onClick={() => setcurrent(index)}
+                    >
+                    {current === index && (
+                        <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+                    )}
+                    </div>
+                ))}
+            </div>
+    </div>
     )
 };
+
+
+                //deux div de trop
+
 
 export default Slider
