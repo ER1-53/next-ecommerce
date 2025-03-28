f0a3945c3522ac583621a3e53d42302ecba71f27
ER1-53 <cremey.erwan@gmail.com>
2025-03-28 10:38:09 +0100

    update the tuto project

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,30 @@
-"use client"
-
 import Link from "next/link"
 import Image from "next/image"
+import { wixClientServer } from "@/lib/wixClientServer"
 
+const PRODUCT_PER_PAGE = 20;
 
-const ProductList = () => {
+const ProductList = async ({
+  categoryId,
+  limit
+}:{
+  categoryId:string;
+  limit?:number
+}) => {
+
+  const wixClient =await wixClientServer()
+
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .find();
+
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%]  lg:w-[22%]">
+      {res.items.map((product:products.product) =>(
+
+        <Link href="/product" className="w-full flex flex-col gap-4 sm:w-[45%]  lg:w-[22%]">
         <div className="relative w-full h-80">
           <Image src="/tomate-1.jpg"
             alt="" fill sizes="25vw"
@@ -25,60 +42,7 @@ const ProductList = () => {
          <div className="text-sm text-gray-500">My description</div>
          <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
       </Link>
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="relative w-full h-80">
-          <Image src="/tomate-1.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500">
-            </Image>
-            <Image src="/tomate-2.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md">
-          </Image>
-         </div>
-         <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$45</span>
-         </div>
-         <div className="text-sm text-gray-500">My description</div>
-         <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
-      </Link>
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="relative w-full h-80">
-          <Image src="/tomate-1.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500">
-            </Image>
-            <Image src="/tomate-2.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md">
-          </Image>
-         </div>
-         <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$45</span>
-         </div>
-         <div className="text-sm text-gray-500">My description</div>
-         <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
-      </Link>
-      <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-        <div className="relative w-full h-80">
-          <Image src="/tomate-1.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500">
-            </Image>
-            <Image src="/tomate-2.jpg"
-            alt="" fill sizes="25vw"
-            className="absolute object-cover rounded-md">
-          </Image>
-         </div>
-         <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$45</span>
-         </div>
-         <div className="text-sm text-gray-500">My description</div>
-         <button className="rounded-2xl ring-1 ring-lama text-lama w-max py-2 px-4 text-xs hover:bg-lama hover:text-white ">Add to Cart</button>
-      </Link>
+            ) )}
     </div>
   )
 }
